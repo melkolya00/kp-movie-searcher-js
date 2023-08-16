2c7b26f77017bd865058ab72b2df7011c9f9e290
Николай Мелеховец <melehkola@gmail.com>
2023-08-16 16:33:25 +0300

    rate

---
+++ elements.js
@@ -0,0 +1,8 @@
+export const hintElem = document.querySelector('#hint');
+export const resultBlock = document.querySelector('.result--block');
+export const backgroundElem = resultBlock.querySelector('.result--background');
+export const movieTitle = document.getElementById("movieTitle");
+export const movieRating =  document.getElementById("movieRating");
+export const movieGenres = document.getElementById("movieGenres");
+export const moviePoster =  document.getElementById("moviePoster");
+export const movieDesc =  document.getElementById("movieDesc");
