2b27fe0c9da3c65c6d18cc80d228d637ec732c25
Николай Мелеховец <melehkola@gmail.com>
2023-08-17 13:58:26 +0300

    fix

--- country_flags.js
+++ country_flags.js
@@ -1,38 +1,39 @@
-import { getMovieCountries } from './kinopoisk_search.js';
+import { getMovieCountries } from "./kinopoisk_search.js";
 let countryNameToCodeMap = {};
 let countriesLoaded = false;
 
 async function loadCountries() {
-    try {
-        // 1. Загрузка локального XML файла
-        const response = await fetch('./codes.xml');
-        if (!response.ok) {
-            throw new Error(`Ошибка при загрузке XML: ${response.statusText}`);
-        }
-        const xmlText = await response.text();
-
-        // 2. Преобразование текста в объект Document
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlText, "text/xml");
-
-        // 3. Анализ объекта Document и наполнение словаря
-        const countries = xmlDoc.querySelectorAll("country");
-        countries.forEach(country => {
-            const name = country.querySelector("name").textContent;
-            const alpha2 = country.querySelector("alpha2").textContent;
-            countryNameToCodeMap[name] = alpha2;
-        });
-
-    } catch (error) {
-        console.error("Ошибка при загрузке или анализе XML:", error);
+  try {
+    // 1. Загрузка локального XML файла
+    const response = await fetch("./codes.xml");
+    if (!response.ok) {
+      throw new Error(`Ошибка при загрузке XML: ${response.statusText}`);
     }
+    const xmlText = await response.text();
+
+    // 2. Преобразование текста в объект Document
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+
+    // 3. Анализ объекта Document и наполнение словаря
+    const countries = xmlDoc.querySelectorAll("country");
+    countries.forEach((country) => {
+      const name = country.querySelector("name").textContent;
+      const alpha2 = country.querySelector("alpha2").textContent;
+      countryNameToCodeMap[name] = alpha2;
+    });
+  } catch (error) {
+    console.error("Ошибка при загрузке или анализе XML:", error);
+  }
 }
 
 export async function getCountryCodes() {
-    if (!countriesLoaded) {
-        await loadCountries();
-        countriesLoaded = true;
-    }
-    const currentMovieCountries = getMovieCountries();
-    return currentMovieCountries.map(country => countryNameToCodeMap[country] || 'Unknown');
+  if (!countriesLoaded) {
+    await loadCountries();
+    countriesLoaded = true;
+  }
+  const currentMovieCountries = getMovieCountries();
+  return currentMovieCountries.map(
+    (country) => countryNameToCodeMap[country] || "Unknown"
+  );
 }
--- kinopoisk_search.js
+++ kinopoisk_search.js
@@ -1,107 +1,112 @@
-import { API_KEY } from './config.js';
-import { getCountryCodes } from './country_flags.js';
-import * as elements from './elements.js';
-
+import { API_KEY } from "./config.js";
+import { getCountryCodes } from "./country_flags.js";
+import * as elements from "./elements.js";
 let movieCountriesArr = [];
 const headers = {
-    "X-API-KEY": API_KEY
+  "X-API-KEY": API_KEY,
 };
-
 function setRatingColor(rating, element) {
-    element.style.color = rating < 5.0 ? "red" :
-                          rating < 7.0 ? "yellow" : "green";
+  element.style.color =
+    rating < 5.0 ? "red" : rating < 7.0 ? "yellow" : "green";
 }
-
 async function getMoviesByName(name, page = 1, limit = 1) {
-    try {
-        const response = await fetch(`https://api.kinopoisk.dev/v1.2/movie/search?${new URLSearchParams({
-            "query": name,
-            "limit": limit,
-            "page": page,
-        })}`, { headers });
+  try {
+    const response = await fetch(
+      "https://api.kinopoisk.dev/v1.2/movie/search?" +
+        new URLSearchParams({
+          query: name,
+          limit: limit,
+          page: page,
+        }),
+      {
+        headers: headers,
+      }
+    );
 
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        const movies = await response.json();
-        return movies.docs;
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-}
 
+    const movies = await response.json();
+    return movies.docs;
+  } catch (error) {
+    console.error(error);
+  }
+}
 document.addEventListener("keyup", showResults);
 
 function showResults(event) {
-    if (event.code.toLowerCase() === "enter") {
-        const filmName = document.querySelector('#title').value.trim();
-        if (!filmName) {
-            showMessage('Введите название фильма.');
-            return;
-        }
-        getMoviesByName(filmName).then(async movies => {
-            if (movies.length === 0) {
-                showMessage('Такого фильма нет на Кинопоиске.');
-            } else {
-                movies.forEach(async movie => {
-                    updateUI(movie);
-                    movieCountriesArr = movie.countries;
-                });
-            }
+  if (event.code.toLowerCase() === "enter") {
+    elements.movieCountries.innerHTML = "";
+    const filmName = document.querySelector("#title").value.trim();
+    if (!filmName) {
+      // проверяем пустая ли строка
+      showMessage("Введите название фильма.");
+      return;
+    }
+    getMoviesByName(filmName).then(async (movies) => {
+      if (movies.length === 0) {
+        showMessage("Такого фильма нет на Кинопоиске.");
+      } else {
+        movies.forEach(async (movie) => {
+          updateUI(movie);
+          movieCountriesArr = movie.countries;
+          const countryCodesForThisMovie = await getCountryCodes();
+          updateFlags(countryCodesForThisMovie);
         });
-    }
-}
-function showMessage(msg) {
-    elements.hintElem.innerHTML = msg;
-    elements.hintElem.style.opacity = 1;
-    setTimeout(() => {
-        elements.hintElem.style.opacity = 0;
-    }, 2000);
-}
-async function updateUI(movie) {
-    elements.backgroundElem.style.backgroundImage = `url(${movie.backdrop})`;
-    setTimeout(() => {
-        elements.backgroundElem.classList.add('loaded');
-    }, 1000);
-    elements.backgroundElem.style.filter = "blur(30px)";
-    elements.movieTitle.innerText = `${movie.name} (${movie.year})`;
-    elements.movieRating.innerText = movie.rating;
-    elements.movieGenres.innerText = movie.genres.join(", ");
-    elements.movieLength.innerText = `${movie.movieLength} мин. / ${Math.floor(movie.movieLength / 60)} ч. ${movie.movieLength % 60} мин.`;
-
-    getCountryCodes().then(codes => {
-        updateFlags(codes);
+      }
     });
+  }
+}
 
-    elements.moviePoster.style.opacity = 0;
-    elements.moviePoster.src = movie.poster;
-    elements.moviePoster.onload = () => {
-        setTimeout(() => {
-            elements.moviePoster.style.opacity = 1;
-        }, 500);
-    };
-
-    elements.movieDesc.innerText = movie.description;
+function updateUI(movie) {
+  elements.backgroundElem.style.backgroundImage = `url(${movie.backdrop})`;
+  setTimeout(() => {
+    elements.backgroundElem.classList.add("loaded");
+  }, 1000);
+  elements.backgroundElem.style.filter = "blur(30px)";
+  elements.movieTitle.innerText = `${movie.name} (${movie.year})`;
+  elements.movieRating.innerText = movie.rating;
+  elements.movieGenres.innerText = movie.genres.join(", ");
+  elements.movieLength.innerText = `${movie.movieLength} мин. / ${Math.floor(
+    movie.movieLength / 60
+  )} ч. ${movie.movieLength % 60} мин.`;
+  elements.moviePoster.style.opacity = 0;
+  elements.moviePoster.src = movie.poster;
+  elements.moviePoster.onload = () => {
     setTimeout(() => {
-        elements.movieDesc.style.opacity = 1;
+      elements.moviePoster.style.opacity = 1;
     }, 500);
-
-    if (!movie.poster) {
-        showMessage('У данного фильма нет постера.');
-    }
-    else if (!movie.description) {
-      showMessage('У данного фильма нет описания.');
+  };
+  elements.movieDesc.innerText = movie.description;
+  setTimeout(() => {
+    elements.movieDesc.style.opacity = 1;
+  }, 500);
+  if (!movie.poster) {
+    showMessage("У данного фильма нет постера.");
+  }
+  if (!movie.description) {
+    showMessage("У данного фильма нет описания.");
   }
 
-    setRatingColor(movie.rating, elements.movieRating);
+  setRatingColor(movie.rating, elements.movieRating);
 }
 
 function updateFlags(codes) {
-//   elements.movieCountries.innerHTML = movie.countries.join(' ');
-    const flagImages = codes.map(code => {
-        return `<img src="https://flagsapi.com/${code.toUpperCase()}/shiny/16.png" alt="${code} flag" title="${code}" />`;
-    });
-    elements.movieCountries.innerHTML = flagImages.join(' ');
+  const flagImages = codes.map((code) => {
+    return `<img src="https://flagsapi.com/${code.toUpperCase()}/shiny/32.png" alt="${code} flag" title="${code}" />`;
+  });
+  elements.movieCountries.innerHTML = flagImages.join(" ");
+}
+
+function showMessage(msg) {
+  elements.hintElem.innerHTML = msg;
+  elements.hintElem.style.opacity = 1;
+  setTimeout(() => {
+    elements.hintElem.style.opacity = 0;
+  }, 2000);
 }
 
 export function getMovieCountries() {
-    return movieCountriesArr;
+  return movieCountriesArr;
 }
